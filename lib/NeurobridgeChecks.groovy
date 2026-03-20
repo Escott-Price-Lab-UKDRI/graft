9b71ea5bdba199d6643c320ac97ea32bc11c1faf
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2026-03-20 19:13:05 +0000

    Delete lib directory

--- lib/NeurobridgeChecks.groovy
+++
@@ -1,8 +0,0 @@
-class NeurobridgeChecks {
-
-    static void requireParam(value, String name) {
-        if( value == null || value.toString().trim() == '' ) {
-            throw new IllegalArgumentException("Missing --${name}")
-        }
-    }
-}
